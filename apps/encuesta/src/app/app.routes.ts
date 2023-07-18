450bba9f5b77c430c6dd3afaa02097c10ee6193f
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-07-18 06:20:57 -0600

    created encuesta app final

--- apps/encuesta/src/app/app.routes.ts
+++ apps/encuesta/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
     {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/app/home',
+    },
+    {
         path:'app',
         loadChildren:() => 
         import('./pages/pages.module').then((m)=> m.PagesModule), 
